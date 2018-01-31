36381cb9a2ae9593617d0f82348e59f14b5e2f21
akshay <you@example.com>
2018-01-31 16:06:43 +0530

    editing completed

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -18,17 +18,50 @@ window.onload = function () {
     var btntext = document.createTextNode("DELETE");
     btn.appendChild(btntext);
     btn.className = "deletebtn";
-    li.onclick = function () {
+    // li.onclick=()=>{
+    //   localStorage.removeItem(n);
+    //   document.getElementById(n).style.display="none"
+    // }
+    // localStorage.clear();
+    btn.onclick = function () {
+      console.log(localStorage.getItem(n));
       localStorage.removeItem(n);
       document.getElementById(n).style.display = "none";
     };
-    btn.onclick = function () {
-      localStorage.removeItem(n);
-      document.getElementById(n).style.display = "none";
+    var editbtn = document.createElement("BUTTON");
+    var editbtntext = document.createTextNode("EDIT");
+    editbtn.appendChild(editbtntext);
+    editbtn.onclick = function () {
+      var div = document.createElement("li");
+      div.setAttribute("id", n);
+      var editabletextarea = document.createElement("textarea");
+      editabletextarea.setAttribute("id", n);
+      editabletextarea.appendChild(document.createTextNode(n));
+      // editabletextarea.className="editablecontent"
+      var savebtn = document.createElement("BUTTON");
+      savebtn.appendChild(document.createTextNode("SAVE"));
+      savebtn.onclick = function () {
+        console.log("nn", n);
+        console.log("editabletextarea.value", editabletextarea.value);
+        var newtext = editabletextarea.value;
+        console.log("newtext", newtext);
+        localStorage.removeItem(n);
+        localStorage.setItem(newtext, newtext);
+        btn.setAttribute("id", newtext);
+        li.setAttribute("id", newtext);
+        document.getElementById(n).replaceWith(li);
+        n = newtext;
+      };
+      // div.className="textarea-container"
+      div.appendChild(editabletextarea);
+      div.appendChild(savebtn);
+      console.log(div);
+      document.getElementById(n).replaceWith(div);
     };
     var t = document.createTextNode(n);
     li.appendChild(t);
     li.appendChild(btn);
+    li.appendChild(editbtn);
     li.setAttribute("id", n);
     document.getElementById("myul").appendChild(li);
   });
@@ -52,15 +85,21 @@ var add = function add() {
     var editbtntext = document.createTextNode("EDIT");
     editbtn.appendChild(editbtntext);
     editbtn.onclick = function () {
-      var div = document.createElement("div");
+      var div = document.createElement("li");
       var editabletextarea = document.createElement("textarea");
+      editabletextarea.setAttribute("id", x);
       editabletextarea.appendChild(document.createTextNode(x));
-      editabletextarea.className = "editablecontent";
+      // editabletextarea.className="editablecontent"
       var savebtn = document.createElement("BUTTON");
       savebtn.appendChild(document.createTextNode("SAVE"));
-      savebtn.className = "save";
-      div.appendChild(savebtn);
+      savebtn.onclick = function () {
+        var newtext = document.getElementById(x).value;
+        localStorage.removeItem(x);
+        localStorage.setItem(newtext, newtext);
+      };
+      // div.className="textarea-container"
       div.appendChild(editabletextarea);
+      div.appendChild(savebtn);
       console.log(div);
       document.getElementById(x).replaceWith(div);
     };
